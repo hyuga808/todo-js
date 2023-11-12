7e8cf5c77cf504446263be595fc1e88ee5be0f62
Hyuga <123480831+hyuga808@users.noreply.github.com>
2023-11-12 12:00:23 +0000

    Update 4 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,19 @@
 import "./styles.css";
 
+const onclicikAdd = () => {
+  // テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  console.log(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onclicikAdd());
